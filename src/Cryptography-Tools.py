5b29218814b97928dc449963cf35a94d3b266861
Farid78CE <farid.zaredar@gmail.com>
2024-05-14 16:52:22 +0330

    first commit

---
+++ src/Cryptography-Tools.py
@@ -0,0 +1,14 @@
+
+class CryptoTools: 
+    def __init__(self):
+        pass
+
+    def AES(self):
+        pass
+
+    def RSA(self):
+        pass
+
+
+if __name__ == '__main__':
+    print("run successfully")
