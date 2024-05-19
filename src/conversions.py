a1f69a4509462549c8bd40e9db4ded76de9668a8
Farid78CE <farid.zaredar@gmail.com>
2024-05-19 11:21:12 +0330

    secure_hash funtion is done - bug fixes

--- src/conversions.py
+++ src/conversions.py
@@ -3,14 +3,22 @@ import typing
 class Conversions:
 
     def str_bytes(self, string: str) -> bytes:       
-        return string.encode()
+        return string.encode("utf-8")
     
     def bytes_str(self, byte:bytes) -> str:
-        return byte.decode('utf-8')
+        return byte.decode("utf-8")
 
     def bytes_hex(self, byte: bytes) -> str:
       return byte.hex()
+  
+    def hex_bytes(self, hex: str) -> bytes:
+        return bytes.fromhex(hex)
 
     def integer_bytes(self, integer:int , length:int = 10) -> bytes:
-        return integer.to_bytes(length, 'big')
+        return integer.to_bytes(length, "big")
+    
+    def bytes_integer(self, byte:bytes) -> int:
+        return int.from_bytes(byte)
+    
+
 
--- src/cryptography-tools.py
+++ src/cryptography-tools.py
@@ -4,13 +4,14 @@ from cryptography.hazmat.backends import default_backend
 import os
 from conversions import Conversions
 import argparse
+from datetime import datetime
 
 # Symmertic Cryptography
 class AES:
     def __init__(self, conv):
         self.conv = conv
 
-    def get_key_iv(self,):
+    def generate_key_iv(self,):
         # it defines the number of bytes for key <256bits>
         key = os.urandom(32)
         # it defines the number of bytes for initialization vector <128bits>
@@ -18,30 +19,11 @@ class AES:
 
         return (key, iv)
 
-    def craft_cipher(self, key, iv):
+    def craft_AES_cipher(self, key, iv):
         # crafting encryption algorithm with its mode 
         # backend function provides a default backend for cryptographic operations, such as encryption and decryption.
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend) 
         return cipher
-    
-
-    def plaintext_source(self,):        
-        parser = argparse.ArgumentParser(description="Specifying the source of plaintext")
-        parser.add_argument('src', type=str, help="Specify Source", choices=['input', 'path'])
-        parser.add_argument('value', type=str, help='plaintext value/File Path Address')
-        args = parser.parse_args()
-
-
-        if args.src == 'input':
-            return self.conv.str_bytes(args.value)
-        elif args.src == 'path':
-            if os.path.exists(args.value):
-                with open(args.value, 'r') as file:
-                    content = file.read()
-                    return self.conv.str_bytes(content)
-            else:
-                print(f"Error: file {args.value} does not exist!")
-
 
     def padding(self, plaintext):
         # specify how many bytes for padding is needed for arbitrary plaintext
@@ -60,7 +42,6 @@ class AES:
         '''
         padded_plaintext = plaintext + padding_values
         return padded_plaintext
-
     
     def unpadding(self, padded_plaintext):
         # get the last element of byten value
@@ -72,38 +53,180 @@ class AES:
         
         plaintext = padded_plaintext[0: real_length]
         return plaintext
-
     
-    def encrypt(self,):
-       
-        (key,  iv)= self.get_key_iv()
-        cipher = self.craft_cipher(key, iv)
+    def encrypt(self, plaintext):
+        (key,  iv)= self.generate_key_iv()
+        cipher = self.craft_AES_cipher(key, iv)
         encryptor = cipher.encryptor()
         # plaintext = b"Secret Messaage must be transmitted securely to the remote"
-        plaintext = self.plaintext_source()
         padded_plaintext = self.padding(plaintext)
         ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
-        print(f"ciphertext: {self.conv.bytes_hex(ciphertext)}")
+        self.store_key(key, iv)
         return (ciphertext, key, iv)
-        
 
     def decrypt(self, ciphertext, key, iv):
-        cipher = self.craft_cipher(key, iv)
+        cipher = self.craft_AES_cipher(key, iv)
         decryptor = cipher.decryptor()
         padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
         plaintext = self.unpadding(padded_plaintext)
-        print(f"plaintext: {self.conv.bytes_str(plaintext)}")      
         return plaintext
 
-class SHA256:
+    def store_key(self, key, iv):
+        cwd = os.getcwd()
+        full_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        path = cwd + "\\key " + full_time
+        key  = self.conv.bytes_hex(key); iv = self.conv.bytes_hex(iv)
+        try:
+            with open(path, "a") as file:
+                file.write(key); file.write("\n") ; file.write(iv)
+        except Exception as e:
+            print(e)
+            
+class SecureHash:
     def __init__(self, conv):
         self.conv = conv
         
-    def hash():
-        argparse.ArgumentParser(description="")
-        hashes.SHA256
+    def select_hash_func(self, hash_type):
         
+        digest: hashes.Hash
+        
+        if hash_type == "MD5":
+            digest = hashes.Hash(hashes.MD5(),backend=default_backend)
+        elif hash_type == "SHA1":
+            digest = hashes.Hash(hashes.SHA1(),backend=default_backend)
+        elif hash_type == "SHA384":
+            digest = hashes.Hash(hashes.SHA384(),backend=default_backend)
+        elif hash_type == "SHA512":
+            digest = hashes.Hash(hashes.SHA512(),backend=default_backend)
+        elif hash_type == "SHA3_256":
+            digest = hashes.Hash(hashes.SHA3_256(),backend=default_backend)
+        elif hash_type == "SHA3_384":
+            digest = hashes.Hash(hashes.SHA3_384(),backend=default_backend)
+        elif hash_type == "SHA3_512":
+            digest = hashes.Hash(hashes.SHA3_512(),backend=default_backend)
+        else:
+            digest = hashes.Hash(hashes.SHA256(),backend=default_backend)
+            
+        return digest
+    
+    def cal_hash(self, path_or_data, hash_type, flag, chunck_size=8192):
+        if flag:
+            digest = self.select_hash_func(hash_type)
+            with open(path_or_data, 'rb') as file:
+                while chunck := file.read(chunck_size):
+                    digest.update(chunck)
 
+            hash_value = digest.finalize()
+            return hash_value
+        else:
+            digest = self.select_hash_func(hash_type)
+            digest.update(path_or_data)
+            hash_value = digest.finalize()
+            return hash_value
+
+    def compare(self, ):
+        pass
+    
+class CommandHandler:
+    def AES_handler(self):
+        pass
+    
+    def secure_hash_handler(self):
+        pass
+    
+    def main(self):
+        """
+        formatter_class: Allows customization of the help message formatting. 
+        argparse.ArgumentDefaultsHelpFormatter is used here to include default values in the help message.
+        """
+        parser = argparse.ArgumentParser(
+            description="Cryptographic tool",
+            epilog="Primitive cryptographic functions that is widely used",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        
+        subparsers = parser.add_subparsers(dest="command", help="commands", description="All available commands")
+        
+        # AES commands 
+        AES_parser = subparsers.add_parser("aes", description="AES256-CBC mode", help="AES encryption algorithm.")
+        AES_parser.add_argument("cipher_or_decipher", type=str, choices=["encrypt", "decrypt"], help="Choose between encryption/decryption.")
+        AES_parser.add_argument("path_or_data_enc", type=str, help="Plaintext value/file address")
+        AES_parser.add_argument("--credentials", type=str, help="Credentials (key, iv) path")
+        
+        
+        
+        # SecureHash commands
+        secure_hash_parser = subparsers.add_parser("secure_hash", description="Using different Hash function for getting message digest", help="Secure Hash functions")
+        secure_hash_parser.add_argument("hash_type", type=str, choices=["MD5", "SHA1", "SHA256", "SHA384", "SHA512", "SHA3_256", "SHA3_384", "SHA3_512"])
+        secure_hash_parser.add_argument("path_or_data_hash", type=str, help="plaintext value/File Address")
+        secure_hash_parser.add_argument("--compare", type=str, help="compare calculated hash with official published hash")
+        
+          
+        arg = parser.parse_args()
+        
+        conv = Conversions()
+        
+        # AES module
+        if arg.command == "aes":
+            aes = AES(conv)
+            if arg.cipher_or_decipher == "encrypt":
+                if os.path.exists(arg.path_or_data_enc):
+                    with open(arg.path_or_data_enc, 'r') as file:
+                        plaintext = file.read()
+                        plaintext = conv.str_bytes(plaintext)
+                        ciphertext, key, iv = aes.encrypt(plaintext)
+                        print(f"ciphertext: {conv.bytes_hex(ciphertext)}")
+                else:
+                    plaintext = conv.str_bytes(arg.path_or_data_enc)
+                    ciphertext, key, iv = aes.encrypt(plaintext)
+                    print(f"ciphertext: {conv.bytes_hex(ciphertext)}")
+            else:
+                # decryption
+                # D:\Eternal_Liberty_And_Glory\Source Codes\Cryptography\Cryptography-Utils\src\key 2024-05-17 22-36-16
+                if arg.credentials is not None:
+                    if os.path.exists(arg.credentials):
+                        with open(arg.credentials, 'r') as file:
+                            content = file.read().split("\n")
+                            key = conv.hex_bytes(content[0]) ; iv = conv.hex_bytes(content[1])
+                            if os.path.exists(arg.path_or_data_enc):
+                                with open(arg.path_or_data_enc) as f:
+                                    ciphertext = f.read()
+                                    ciphertext = conv.hex_bytes(ciphertext)
+                                    plaintext = aes.decrypt(ciphertext, key, iv)
+                                    print(f"Plaintext: {conv.bytes_str(plaintext)}")
+                            else:
+                                ciphertext = conv.hex_bytes(arg.path_or_data_enc)
+                                plaintext = aes.decrypt(ciphertext, key, iv)
+                                print(f"Plaintext: {conv.bytes_str(plaintext)}")
+                    else:
+                        print("Path not exist!")
+                else:
+                    print("You must use --credential option for specifying the key & iv path")
+        
+        # Secure Hash module         
+        elif arg.command == "secure_hash":
+            
+            secure_hash = SecureHash(conv)
+            hash_type = arg.hash_type
+            chunck_size = 8192 # specify the chunck size (in byte) for reading files
+            flag = False # if flag=T it is a path to read file 
+            
+            if os.path.exists(arg.path_or_data_hash):
+                flag = True 
+                hash_value = secure_hash.cal_hash(arg.path_or_data_hash, hash_type, flag, chunck_size)
+                hash_value = conv.bytes_hex(hash_value)
+                print(f"Hash Value: {hash_value}")
+            else:
+                flag = False
+                # print(f"Data: {arg.path_or_data_hash}, type: {type(arg.path_or_data_hash)}")
+                plaintext = conv.str_bytes(arg.path_or_data_hash)
+                hash_value = secure_hash.cal_hash(plaintext, hash_type, flag, chunck_size)
+                hash_value = conv.bytes_hex(hash_value)
+                print(f"Hash Value: {hash_value}")
+
+            if arg.compare:
+                pass
+        
+        
 # # Assymetric Cryptography
 # class RSA:
 #     def __init__(self):
@@ -111,7 +234,27 @@ class SHA256:
 
 
 if __name__ == '__main__':
-    conv = Conversions()
-    aes = AES(conv)
-    (ciphertext, key, iv) =  aes.encrypt()
-    aes.decrypt(ciphertext, key, iv)
+    command = CommandHandler()
+    command.main()
+
+
+
+"""
+aes usage example: 
+    
+    -- encryption 
+    python .\cryptography-tools.py aes 
+    encrypt "D:\Eternal_Liberty_And_Glory\Source Codes\Cryptography\Cryptography-Utils\src\Statement.txt"
+    
+    -- decryption
+    python .\cryptography-tools.py aes 
+    decrypt "D:\Eternal_Liberty_And_Glory\Source Codes\Cryptography\Cryptography-Utils\src\Statement_enc.txt" 
+    --credentials "D:\Eternal_Liberty_And_Glory\Source Codes\Cryptography\Cryptography-Utils\src\key 2024-05-18 16-42-04"
+
+
+secure_hash usage example: 
+    -- SHA256
+    python .\cryptography-tools.py secure_hash SHA256 "HelloWorld!"
+    -- SHA512
+    python .\cryptography-tools.py secure_hash SHA512 "HelloWorld!"
+"""
